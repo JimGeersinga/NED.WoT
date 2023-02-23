2b613941c3320d3ccd6cd57c64b7a85a8c2415e5
Jim Geersinga <jim@huss.nl>
2023-02-23 10:41:54 +0100

    Add version

---
+++ NED.WoT.BattleResults.Client/Attributes/DirectoryExistsAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NED.WoT.BattleResults.Client.Attributes
+{
+    public class DirectoryExistsAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return !string.IsNullOrWhiteSpace(value.ToString()) && Directory.Exists(value.ToString());
+        }
+    }
+}
