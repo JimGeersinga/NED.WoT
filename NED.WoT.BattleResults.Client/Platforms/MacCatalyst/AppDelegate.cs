d717f687efe2b2f29ba0c27be099404923cef31c
Jim Geersinga <j.geersinga@outlook.com>
2025-03-22 22:08:09 +0100

    remove unused resources

--- NED.WoT.BattleResults.Client/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace NED.WoT.BattleResults.Client;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
